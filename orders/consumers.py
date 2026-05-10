416c3bb974844780de14969393f1a339a6e6f69b
bea <beatussimon47@gmail.com>
2026-05-11 01:53:37 +0300

    fix(ws): allow unauthenticated access to store order channels for public TV display

--- orders/consumers.py
+++ orders/consumers.py
@@ -5,18 +5,21 @@ from channels.db import database_sync_to_async
 class OrderConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         user = self.scope.get("user")
-        if not user or not user.is_authenticated:
-            await self.close(code=4001)
-            return
-
+        
         self.store_id = self.scope['url_route']['kwargs'].get('store_id')
         self.order_id = self.scope['url_route']['kwargs'].get('order_id')
         
+        # If specific order tracking, require auth or check session (optional strictness)
         if self.order_id:
+            if not user or not user.is_authenticated:
+                await self.close(code=4001)
+                return
             self.room_group_name = f'order_{self.order_id}'
         elif self.store_id:
+            # PUBLIC TV ACCESS: Allow store channels without login
             self.room_group_name = f'store_{self.store_id}_orders'
         else:
+            # PUBLIC GLOBAL ACCESS: Allow global updates without login
             self.room_group_name = 'global_orders'
 
         # Join room group
