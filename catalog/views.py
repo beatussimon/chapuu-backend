84767ef2dad8bb457e9b063e962c6369bf052f39
bea <beatussimon47@gmail.com>
2026-05-27 22:27:42 +0300

    chore(views): print validation errors during product creation/update for debugging

--- catalog/views.py
+++ catalog/views.py
@@ -91,6 +91,25 @@ class ProductViewSet(viewsets.ModelViewSet):
             
         return queryset
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            print("PRODUCT CREATION VALIDATION ERROR:", serializer.errors)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if not serializer.is_valid():
+            print("PRODUCT UPDATE VALIDATION ERROR:", serializer.errors)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
     def perform_create(self, serializer):
         product = serializer.save()
         self._handle_initial_stock(product)
